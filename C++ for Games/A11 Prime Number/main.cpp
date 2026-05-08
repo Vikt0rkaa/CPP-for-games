ed8a161af87accfa5aa7296d7d01ca09ff238b5f
Vikt0rkaa <vkratka2006@gmail.com>
2026-05-08 22:16:30 +0200

    enhanced prime nums

--- C++ for Games/A11 Prime Number/main.cpp
+++ C++ for Games/A11 Prime Number/main.cpp
@@ -6,6 +6,7 @@ int main()
 {
 	int input;
 	unsigned int num;
+	int max, min;
 
 	std::cout << "Enter a natural number: " << std::endl;
 	std::cin >> input;
@@ -16,6 +17,28 @@ int main()
 		std::cin.clear();
 		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 		std::cin >> input;
+	}
+
+	std::cout << "Enter a min: " << std::endl;
+	std::cin >> min;
+
+	while (std::cin.fail() || min < 0)
+	{
+		std::cerr << "ERROR: Please enter a valid natural number." << std::endl;
+		std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		std::cin >> min;
+	}
+
+	std::cout << "Enter a max: " << std::endl;
+	std::cin >> max;
+
+	while (std::cin.fail() || max < 0 || max <= min )
+	{
+		std::cerr << "ERROR: Please enter a valid natural number." << std::endl;
+		std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		std::cin >> max;
 	}
 
 	num = static_cast<unsigned int>(input);
@@ -44,6 +67,34 @@ int main()
 	else
 	{
 		std::cout << "The number " << num << " is not prime." << std::endl;
+	}
+
+	std::cout << "Prime Numbers in range from " << min << " to " << max << " are: " << std::endl;
+
+	for (int i = min;i <= max;++i)
+	{
+		bool isPrime = true;
+
+		if (i <= 1)
+		{
+			isPrime = false;
+		}
+		else
+		{
+			for (int j = 2;j * j <= i;++j)
+			{
+				if (i % j == 0)
+				{
+					isPrime = false;
+					break; //so we dont need to check every number when we alr found one
+				}
+			}
+		}
+
+		if (isPrime == 1)
+		{
+			std::cout << i << std::endl;
+		}
 	}
 	return 0;
 }
